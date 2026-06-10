6fa66f8684cc6c6f9d84dd1649337deb8752a2b2
Mitchell Avery <averymr@colorado.edu>
2026-06-10 00:17:56 -0600

    Initial OpenGL project setup.  Set up GLFW and GLAD and successfully compiled.  Set up a simple window with buffer swap, callback listener for resizing, and an input processor for handling 'ESC' key to quit.

---
+++ src/01_window/main.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+
+// callback for resizing viewer window
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+// ESC input control
+void processInput(GLFWwindow* window);
+
+int main()
+{
+	int w = 1920;
+	int h = 1080;
+
+	glfwInit(); // initialize GLFW
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4); // configure GLFW (openGL version 3.3 in this case)
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// create window object
+	// this holds all windowing data - required by most of GLFW's other functions
+	// requires width and height as first 2 args, then optional window name.  
+	// returns a GLFWwindow object
+
+	GLFWwindow* window =
+		glfwCreateWindow(w, h, "LearnOpenGL", nullptr, nullptr);  
+
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+
+	// GLAD - manages function pointers for OpenGL
+	// need to initialize before calling any OpenGL funciton
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+
+	// Finally we need to tell openGL the size of the rendering window
+	// first 2 vals set location of lower left corner of window
+	//
+	glViewport(0, 0, w, h);
+
+	// register callback function for window resize
+	// other callbacks possible for e.g. joystic input, process error messages, etc
+	// register callbacks after creating the window and before initiating render loop.
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+	// while loop keeps the window open, swapping back and forth between buffers
+	// continues until explicitely told to stop. without this a single frame would flass and then quit and close.
+	// glfwWindowShouldClose checks at the start of each loop for instructions to close
+	// glfwPollEvents checks if events are triggerd (e.g. keyboard input)
+	while(!glfwWindowShouldClose(window))
+	{
+		processInput(window); // looking for specific ESC key presses
+
+		// rendering commands here
+		//
+		glClearColor(0.8f, 0.1f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// check and call events and swap the buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+
+	// properly clean/delete GLFW's resources
+	glfwTerminate();
+	std::cout << "Goodbye!" << std::endl;
+	return 0;
+
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
